b8f4261136a824f3e045c5bd9c849c05c015d4f9
rakhish <rakhi.ptr@gmail.com>
2021-08-21 19:54:40 +0900

    Add project files.

---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQ { get; set; }
+        public string JokeAns { get; set; }
+    }
+
+   
+}
---
+++ JokesApp/Views/Home/Jokes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApp.Views.Home
+{
+    public class Jokes
+    {
+    }
+}
